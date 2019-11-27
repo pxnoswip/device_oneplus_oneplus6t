6336697fc3067497c4d629fc8590eb9b973a5546
DennySPB <dennyspb@gmail.com>
2019-11-27 15:10:18 +0300

    Re-enable QTI performance usage
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: I3b653eafebcc7e6785738d74a455ea52dcbce4bc
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- device.mk
+++ device.mk
@@ -159,10 +159,6 @@ PRODUCT_PACKAGES += \
     vendor.oneplus.camera.CameraHIDL@1.0-adapter-helper \
     vendor.oneplus.camera.CameraHIDL-V1.0-java
 
-# Power
-PRODUCT_PACKAGES += \
-    power.qcom:64
-
 # Prebuilts
 PRODUCT_PACKAGES += \
     OnePlusCamera \
@@ -201,7 +197,7 @@ PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
 #QTI performance
-#PRODUCT_BOOT_JARS += \
+PRODUCT_BOOT_JARS += \
     QPerformance \
     UxPerformance
 
