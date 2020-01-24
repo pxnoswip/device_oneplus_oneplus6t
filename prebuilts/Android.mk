Vovan77rus59/op6

Build themepicker and pixelthemes
Signed-off-by: drpradsms <dr.pradsms@gmail.com>

prebuilts/Android.mk
@@ -1,4 +1,4 @@
-LOCAL_PATH := $(call my-dir)
+ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := OnePlusCamera
@@ -30,4 +30,24 @@ LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := CallLogManager
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := priv-app/CallLogManager/CallLogManager.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_PATH := $(TARGET_OUT)/app
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := PixelThemes
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := priv-app/PixelThemes/PixelThemes.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_PATH := $(TARGET_OUT)/product/priv-app
 include $(BUILD_PREBUILT)

pxnoswip/device_oneplus_oneplus6t

prebuilts/Android.mk
@@ -31,3 +31,23 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ThemePicker
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := priv-app/ThemePicker/ThemePicker.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_PATH := $(TARGET_OUT)/app
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := PixelThemes
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := priv-app/PixelThemes/PixelThemes.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_PATH := $(TARGET_OUT)/product/priv-app
+include $(BUILD_PREBUILT)