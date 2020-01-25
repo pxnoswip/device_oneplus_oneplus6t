2a1d4245205445785e6b9eab4f9f3f611022bf60
drpradsms <dr.pradsms@gmail.com>
2020-01-25 17:40:21 +0000

    DeviceParts: Build for 6t only
    Signed-off-by: drpradsms <dr.pradsms@gmail.com>

--- Android.mk
+++ Android.mk
@@ -17,6 +17,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter oneplus6 oneplus6t,$(TARGET_DEVICE)),)
+ifneq ($(filter oneplus6t,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
