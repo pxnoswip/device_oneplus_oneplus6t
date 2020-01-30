omnirom/android_device_oneplus_oneplus6t

op6t: Introduce in-screen fingerprint HAL
Change-Id: I0c034def5803ab80e6158a4c4d1caf1eeaa92841

device.mk
@@ -25,5 +25,9 @@ $(call inherit-product, vendor/oneplus/oneplus6t/oneplus6t-vendor.mk)
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,device/oneplus/oneplus6t/prebuilt/system,system)
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    lineage.omni.fingerprint.inscreen@1.0-service.oneplus6t
+
 # common oneplus6
 $(call inherit-product, device/oneplus/oneplus6/device.mk)

pxnoswip/device_oneplus_oneplus6t

Signed-off-by: drpradsms <dr.pradsms@gmail.com>

device.mk
@@ -42,3 +42,7 @@ $(call inherit-product, device/oneplus/sdm845-common/common.mk)
 
 # Inherit from vendor blobs
 #$(call inherit-product, vendor/oneplus/fajita/fajita-vendor.mk)
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    omni.biometrics.fingerprint.inscreen@1.0-service.oneplus6t